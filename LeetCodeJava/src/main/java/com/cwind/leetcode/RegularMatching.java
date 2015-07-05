63a7580bdc1bdd4133fdd3ec35d0f57a66eb8399
Bill Chen <billchen01@163.com>
2015-07-05 09:43:05 +0800

    Java atoi

---
+++ LeetCodeJava/src/main/java/com/cwind/leetcode/RegularMatching.java
@@ -0,0 +1,118 @@
+package com.cwind.leetcode;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Cwind
+ * Date: 7/4/15
+ * Time: 5:08 PM
+ * Email: billchen01@163.com
+ * #10 - Hard
+ */
+public class RegularMatching {
+
+    //Greedy algorithm, but may fail for "aaa", "ab*a*c*a" - DP will solve this.
+    public boolean isMatch(String /* string to check */ s, String /* patterns */ p) {
+        int sindex = 0, pindex=0;
+        for(;pindex<p.length(); pindex++){
+            if(sindex>=s.length()){
+                break;
+            }
+            char cp = p.charAt(pindex);
+            //match {x}*
+            if((pindex<p.length()-1)&&p.charAt(pindex+1)=='*'){
+                if(cp=='.'){ //case: .*
+                    if(pindex+2>=p.length()){ // pattern ends with '.*'
+                        return true;
+                    }else { // find p[pindex+2] in s
+                        char nextc = p.charAt(pindex+2);
+                        boolean found = false;
+                        while(sindex<s.length()){
+                            if(s.charAt(sindex)==nextc){
+                                sindex++;
+                                pindex+=2;
+                                found = true;
+                                break;
+                            } else {
+                                sindex++;
+                            }
+                        }
+                        if(!found){
+                            return false;
+                        }
+                    }
+                }else { // {x}*, x is not '.'
+                    //Should check the characters after '*' to see how many 'x' should there be
+                    int pcount = 0, scount = 0; //scount should be no less than pcount
+                    while((pindex+2+pcount<p.length())&&p.charAt(pindex+2+pcount)==cp){ //Calculate pcount
+                        pcount++;
+                    }
+                    while ((sindex+scount<s.length())&&s.charAt(sindex+scount)==cp) {
+                        scount++;
+                    }
+                    if(scount<pcount){
+                        return false;
+                    }
+
+                    pindex = pindex + pcount + 1;
+                    sindex = sindex + scount;
+                }
+            } else { //Check single char
+                  if(cp=='.'){
+                      sindex++;
+                  }else{
+                      if(s.charAt(sindex)!=cp){
+                          return false;
+                      } else {
+                          sindex++;
+                      }
+                  }
+            }
+
+        }
+        //Patterns ends while still chars in input string
+        if(sindex<s.length()||pindex<p.length()){
+            return false;
+        }
+        return true;
+    }
+   /*
+   '.' Matches any single character.
+   '*' Matches zero or more of the preceding element.
+
+    The matching should cover the entire input string (not partial).
+
+    The function prototype should be:
+    bool isMatch(const char *s, const char *p)
+
+    Some examples:
+    isMatch("aa","a") → false
+    isMatch("aa","aa") → true
+    isMatch("aaa","aa") → false
+    isMatch("aa", "a*") → true
+    isMatch("aa", ".*") → true
+    isMatch("ab", ".*") → true
+    isMatch("aab", "c*a*b") → true
+    */
+    public static void main(String[] args){
+        RegularMatching tester = new RegularMatching();
+        System.out.println(tester.isMatch("aa", "a"));      // false
+        System.out.println(tester.isMatch("aa", "aa"));     // true
+        System.out.println(tester.isMatch("aaa", "aa"));    // false
+        System.out.println(tester.isMatch("aa", "a*"));     // true
+        System.out.println(tester.isMatch("aa", ".*"));     // true
+        System.out.println(tester.isMatch("ab", ".*"));     // true
+        System.out.println(tester.isMatch("aab", "c*a*b")); // true
+        System.out.println(tester.isMatch("acb", "a*.b"));  // true
+        System.out.println(tester.isMatch("aaa", "aaaa"));  // false
+        System.out.println(tester.isMatch("ab", ".*c"));    // false
+        System.out.println(tester.isMatch("aaa", "a*a"));   // true
+        System.out.println(tester.isMatch("aaa", "a*aa"));  // true
+        System.out.println(tester.isMatch("aaa", "a*aaa")); // true
+        System.out.println(tester.isMatch("aaa", "ab*a*c*a"));    // true
+        System.out.println(tester.isMatch("aaaa", "ab*a*c*a"));    // true
+        System.out.println(tester.isMatch("abaaaaaccca", "ab*a*c*a"));    // true
+        System.out.println(tester.isMatch("abca", "ab*a*c*a"));    // true
+        System.out.println(tester.isMatch("aa", "ab*a*c*a"));    // true
+        System.out.println(tester.isMatch("abbbccca", "ab*a*c*a"));    // true
+    }
+}
---
+++ LeetCodeJava/src/main/java/com/cwind/leetcode/StringToInteger.java
@@ -0,0 +1,65 @@
+package com.cwind.leetcode;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Cwind
+ * Date: 7/4/15
+ * Time: 10:25 AM
+ * Email: billchen01@163.com
+ * #8 - Easy
+ */
+public class StringToInteger {
+    public int myAtoi(String str) {
+        if(str==null){
+            return 0;
+        }
+        if(str.equals("")){
+            return 0;
+        }
+        str = str.trim();
+        int result = 1;
+        int sign = 1;
+        if(str.charAt(0)=='-'||str.charAt(0)=='+'){
+            sign = str.charAt(0)=='-'?-1:1;
+            str = str.substring(1);
+        }
+
+        for(int i=0;i<str.length();i++){
+            if(i==0){
+                result = str.charAt(i) - 48;
+                if(result<0||result>9){
+                    return 0;
+                }
+                continue;
+            }
+            int num = str.charAt(i) - 48;
+            if(num < 0 || num > 9){
+                break;
+            }
+            result *= 10;
+            result += num;
+            if(i>=10||result<0){
+                if(sign>0){
+                    return Integer.MAX_VALUE;
+                }else {
+                    return Integer.MIN_VALUE;
+                }
+            }
+        }
+
+        return result*sign;
+    }
+
+    public static void main(String[] args) {
+        StringToInteger tester = new StringToInteger();
+        System.out.println(tester.myAtoi("-123"));
+        System.out.println(tester.myAtoi("+-123"));
+        System.out.println(tester.myAtoi("    010"));
+        System.out.println(tester.myAtoi("  -0012a42"));
+        System.out.println(tester.myAtoi("2147483648"));
+        System.out.println(tester.myAtoi("    10522545459"));
+
+        System.out.println("Max integer= " + Integer.MAX_VALUE);
+        System.out.println("Min integer= " + Integer.MIN_VALUE);
+    }
+}
