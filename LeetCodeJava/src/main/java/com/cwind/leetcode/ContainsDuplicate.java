a1824eb009ac7e0d1f1c4818f682a15916ca5767
BillyChen <billchen01@163.com>
2015-07-05 23:33:00 +0800

    Contains Duplicate

---
+++ LeetCodeJava/src/main/java/com/cwind/leetcode/ContainsDuplicate.java
@@ -0,0 +1,34 @@
+package com.cwind.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Cwind
+ * Date: 2015/7/5
+ * Email: billchen01@163.com
+ * #217 - Easy
+ */
+public class ContainsDuplicate {
+
+    //O(nlogn), not good enough if nums contain many numbers
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> uniqueSet = new HashSet<Integer>();
+
+        for(int i=0; i<nums.length; i++){
+            if(uniqueSet.contains(nums[i])){
+                return true;
+            }else {
+                uniqueSet.add(nums[i]);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        ContainsDuplicate tester = new ContainsDuplicate();
+        int[] nums = {1,3};
+        System.out.println(tester.containsDuplicate(nums));
+    }
+}
