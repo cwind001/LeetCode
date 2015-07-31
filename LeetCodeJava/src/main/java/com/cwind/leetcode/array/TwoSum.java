1d91c5074c782689fc249aad78cb13a283fead9a
BillyChen <billchen01@163.com>
2015-07-31 21:15:40 +0800

    Count and say

--- LeetCodeJava/src/main/java/com/cwind/leetcode/array/TwoSum.java
+++ LeetCodeJava/src/main/java/com/cwind/leetcode/array/TwoSum.java
@@ -1,5 +1,8 @@
 package com.cwind.leetcode.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Cwind
@@ -9,6 +12,13 @@ package com.cwind.leetcode.array;
  */
 public class TwoSum {
 
+    /**
+     * This is very straight forward. Go through the array twice and will surely find the answer.
+     * Time complexity: O(n2)
+     * @param nums
+     * @param target
+     * @return
+     */
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
         for(int i=1; i<=nums.length-1; i++){
@@ -23,15 +33,40 @@ public class TwoSum {
         return result;
     }
 
+    /**
+     * Time complexity: O(n), Space complexity: O(n)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        Map map = new HashMap<Integer, Integer>();
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], i+1);
+        }
+
+        for(int i=1; i<=nums.length; i++){
+            if(map.containsKey(target-nums[i-1])&&((Integer)map.get(target-nums[i-1])>i)){
+                return new int[] {i, (Integer)map.get(target-nums[i-1])};
+            }
+        }
+
+        return new int[0];
+    }
+
     public static void main(String[] args){
         int[] nums = {2, 7, 11, 15};
         int target = 9;
         TwoSum tester = new TwoSum();
-        int[] indexes = tester.twoSum(nums, target);
+        int[] indexes = tester.twoSum2(nums, target);
         System.out.println("index1="+indexes[0]+", index2="+indexes[1]);
         int[] nums2 = {3, 2, 4};
         target = 6;
-        indexes = tester.twoSum(nums2, target);
+        indexes = tester.twoSum2(nums2, target);
+        System.out.println("index1="+indexes[0]+", index2="+indexes[1]);
+        int[] nums3 = {0, 4, 3, 0};
+        target = 0;
+        indexes = tester.twoSum2(nums3, target);
         System.out.println("index1="+indexes[0]+", index2="+indexes[1]);
     }
 }
--- LeetCodeJava/src/main/java/com/cwind/leetcode/string/CountAndSay.java
+++ LeetCodeJava/src/main/java/com/cwind/leetcode/string/CountAndSay.java
@@ -8,21 +8,28 @@ package com.cwind.leetcode.string;
  * #38 - Easy
  */
 public class CountAndSay {
-    public String countAndSay(int n) {
-        String tmp = String.valueOf(n);
+
+    /**
+     * Wrong answer - because of misunderstanding. The problem is not to count the parameter and say it,
+     * but to find the nth element in the sequence. "Count and say" itself is a sequence, the nth element is the 'count and say' of (n-1)th element.
+     * @param n
+     * @return
+     */
+    private String countAndSayCurNum(String n) {
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<tmp.length();){
+        for(int i=0; i<n.length();){
             int count = 1;
-            char cur = tmp.charAt(i);
-            if(i==tmp.length()-1){
+            char cur = n.charAt(i);
+            if(i==n.length()-1){
                 sb.append(count).append(cur);
                 break;
             }else {
                 i++;
-                while(tmp.charAt(i)==cur&&i<tmp.length()){
+                while(i<n.length()&&n.charAt(i)==cur){
                     count++;
                     i++;
                 }
+
             }
             sb.append(count).append(cur);
         }
@@ -30,9 +37,22 @@ public class CountAndSay {
         return sb.toString();
     }
 
+    public String countAndSay(int n) {
+        if(n<=0){
+            return "";
+        }
+        if(n==1){
+            return "1";
+        }else {
+            return countAndSayCurNum(countAndSay(n-1));
+        }
+    }
+
     public static void main(String[] args){
         CountAndSay tester = new CountAndSay();
-        System.out.println(tester.countAndSay(111221));
+        System.out.println(tester.countAndSay(1));
+        System.out.println(tester.countAndSay(5));
+        System.out.println(tester.countAndSay(10));
         System.out.println(tester.countAndSay(11));
     }
 }
